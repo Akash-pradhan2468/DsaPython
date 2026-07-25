0f0c6745fc7a5b02fd9dbf3b56ced4f0b7c74961
Akash-pradhan2468 <akash2468.pradhan@gmail.com>
2026-07-25 12:41:25 +0530

    Code compleated

--- Hashing.py
+++ Hashing.py
@@ -1,1 +1,20 @@
-print("hekki")
+import random
+length=random.randint(1,20)
+n=[random.randint(1,100) for i in range(length)]
+m=[random.randint(1,100) for i in range(length)]
+print(n)
+print(m)
+
+def return_count_function(list1,list2):
+    freq={}
+    for i in list1:
+        freq[i]=freq.get(i,0)+1
+
+    ans_freq={}
+    for i in list2:
+        for i in list2:
+            ans_freq[i]=freq.get(i,0)
+
+    return ans_freq
+
+print(f"n={n}\nm={m}\nfrequency values of m in n are\n{return_count_function(n,m)} ")
